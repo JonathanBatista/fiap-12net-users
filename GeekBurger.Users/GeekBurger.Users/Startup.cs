264003a7f6ed54c434a6f5e6a0d6acf1d95c7b5e
jonabat <jonathan.batistasilva@outlook.com>
2018-08-02 22:24:27 -0300

    dependencias

--- GeekBurger.Users/GeekBurger.Users/Startup.cs
+++ GeekBurger.Users/GeekBurger.Users/Startup.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Builder;
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.DependencyInjection;
 
@@ -15,6 +16,7 @@ namespace GeekBurger.Users
                                             .AddJsonFormatters()
                                             .AddCors();
 
+            services.AddAutoMapper();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
